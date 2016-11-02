c1821042b912692bdf163bc19ff4eea8c8cff624
Alvaro Flores and Nhat Hoang <nhat@>
2016-11-02 16:43:08 -0700

    console log product.get(cost) price to totalcost.

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -4,7 +4,6 @@ export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
   actions: {
     addToCart(product){
-      console.log(product);
       this.get('shoppingCart').add(product);
     },
   }
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   products: [],
+  totalcost: 0,
   add(product) {
-    console.log(product.title);
     this.get('products').pushObjects([product]);
-  }
+    console.log(parseInt(product.get('cost')));
+    this.get('totalcost').add(parseInt(product.get('cost')));
+    console.log(totalcost);
+  },
+
 });
